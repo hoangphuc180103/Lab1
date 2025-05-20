fff2634b3272857df87ad6f015889e36c7ab5b0e
CEL Soft <phuchoangnguyen18012003@gmail.com>
2025-05-20 15:57:08 +0700

    bài tập 2 login :email là phucnguyen.180103@gmail.com và pass là 123456789, ngày 20/5

---
+++ app/src/main/java/com/example/lab1/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.lab1
+
+class LoginActivity {
+}
--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.lab1
 
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,8 +20,29 @@ import com.example.lab1.ui.theme.Lab1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
+        val edtEmail = findViewById<EditText>(R.id.edtEmail)
+        val edtPassword = findViewById<EditText>(R.id.edtPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString().trim()
+            val password = edtPassword.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show()
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this, "Email không hợp lệ", Toast.LENGTH_SHORT).show()
+            } else if (email == "phucnguyen.180103@gmail.com" && password == "123456789") {
+                Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Email hoặc Password sai", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
 
